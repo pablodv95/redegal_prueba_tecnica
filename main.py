69c10c4485dfe306c479dd5415459a60970eeb9a
Pablo Díaz Vidal <pdvidal@gti.uvigo.es>
2022-04-21 21:28:44 +0200

    Final version

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import operator
 import sys
 
 def process_data(data_file_name, taxi_zones_file_name):
-    #data_file_name = 'yellow_tripdata_2021-01.csv'
-    #taxi_zones_file_name = 'taxi_zones.csv'
-    df_data = pd.read_csv(data_file_name)
-    df_zones = pd.read_csv(taxi_zones_file_name)
-
-    quantile = df_data.trip_distance.quantile(0.95)
-    #Comentar que existen ID de zona mayores a 263
-    df_filtered_data = df_data[(df_data['DOLocationID'] <= 263) & (df_data['trip_distance'] > quantile)]
+    # Variable definition
     DOLocationID_dict = {}
-    count = 0
+    trips_list = list()
+    borough_list = list()
+    zone_list = list()
 
+    # Get input data (trip data and taxi zone data)
+    df_trips_data = pd.read_csv(data_file_name)
+    df_taxi_zones = pd.read_csv(taxi_zones_file_name)
+
+    # percentile calculation
+    quantile = df_trips_data.trip_distance.quantile(0.95)
+
+    # Data filters: 1- Filter data with taxi zone greater than 263 (maximum zone ID); 2- Filter data with trip distance less than percentile 0,95 for trip distance
+    df_filtered_data = df_trips_data[(df_trips_data['DOLocationID'] <= 263) & (df_trips_data['trip_distance'] > quantile)]
+
+    # Iterate over filtered data to calculate number of trip drop-off inside each zone
     for index, row in df_filtered_data.iterrows():
         if row.DOLocationID not in DOLocationID_dict:
             DOLocationID_dict[row.DOLocationID] = 1
@@ -22,42 +28,38 @@ def process_data(data_file_name, taxi_zones_file_name):
         elif row.DOLocationID in DOLocationID_dict:
             DOLocationID_dict[row.DOLocationID] = DOLocationID_dict[row.DOLocationID] + 1
 
-
-    print("DOLocationID_dict    {}".format(DOLocationID_dict))
+    # Sort number of trips in each zone by descendent order
     sorted_DOLocationID_dict = sorted(DOLocationID_dict.items(), key=operator.itemgetter(1), reverse=True)
-    print("sorted_DOLocationID_dict    {}".format(sorted_DOLocationID_dict))
 
-    trips_list = list()
-    borough_list = list()
-    zone_list = list()
-
+    # Iterate over 10 zones with most trips
     for item in sorted_DOLocationID_dict[:10]:
-
+        # Getting info about borough, zone and number of trips
         location_id = item[0]
         trips_number = item[1]
-        end_borough = df_zones[df_zones.LocationID == location_id].borough.tolist()
-        end_zone = df_zones[df_zones.LocationID == location_id].zone.tolist()
+        end_borough = df_taxi_zones[df_taxi_zones.LocationID == location_id].borough.tolist() # Get borough that matches with trip drop-off location ID
+        end_zone = df_taxi_zones[df_taxi_zones.LocationID == location_id].zone.tolist() # Get zone that matches with trip drop-off location ID
 
+        # Add data to separate lists
         trips_list.append(trips_number)
         borough_list.append(' '.join(end_borough))
         zone_list.append(' '.join(end_zone))
 
-
+    # Put result data in correct format and convert it into a DataFrame
     results = {'Trips': trips_list,
                'end_borough': borough_list,
                'end_zone': zone_list}
+    df_results = pd.DataFrame(results)  #
 
-    df_results = pd.DataFrame(results)
-    #print("RESULTADOS:")
-    #print(df_results)
     return df_results
 
 
 def print_result(result):
+    # Print result data in OS terminal
     print(result)
 
 
 def save_result(result, result_file_name):
+    # Generate csv file with result data to save it
     result.to_csv(result_file_name, index=False)
 
 
@@ -70,17 +72,12 @@ if __name__== "__main__":
         print("Second argument: Taxi zones filename")
         print("Third argument: Result data filename")
         exit(0)
-    '''if sys.argv[1] is None:
-        print("Unknow filename is missing")
-        exit(0)
-    elif sys.argv[2] is None:
-        print("Unknown result filename")
-        exit(0)'''
 
-    #Input parameter
+    #Read input arguments
     file_name = str(sys.argv[1])
     taxi_zones_file_name = str(sys.argv[2])
     result_file_name= str(sys.argv[3])
+
     result = process_data(file_name, taxi_zones_file_name)
     print_result(result)
     save_result(result, result_file_name)
